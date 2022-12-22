6b596d02b38b47925d5b5e8d23eb288854146872
tanishcq <tanishcqmehta@gmail.com>
2022-12-23 01:17:24 +0530

    formController setup

---
+++ src/controllers/formController.js
@@ -0,0 +1,9 @@
+const formModel = require("../models/form");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "FORMAPI";
+
+const saveForm = async (req, res) => {
+    
+}
+
+module.exports = { saveForm };
