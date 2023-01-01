32a0edd9a6663b6c50842e5e02cbafb9d9b55132
tanishcq <tanishcqmehta@gmail.com>
2023-01-01 14:23:42 +0530

    updated form routes

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { saveForm } = require("../controllers/formController");
+const { saveForm, deleteForm, getAllForms } = require("../controllers/formController");
 const formRouter = express.Router();
+const auth = require("../middlewares/auth");
 
-formRouter.post("/save", saveForm);
+formRouter.post("/", auth, saveForm);
+formRouter.get("/", auth, getAllForms);
+formRouter.delete("/:id", auth, deleteForm);
 
 module.exports = formRouter;
