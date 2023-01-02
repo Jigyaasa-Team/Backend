65ca4504a8148a79e174f74ad0b9cfdf3ccfc3c7
tanishcq <tanishcqmehta@gmail.com>
2023-01-02 17:46:01 +0530

    added resetPassword route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signin, signup, reportBugs } = require("../controllers/userController");
+const { signin, signup, reportBugs, resetPassword } = require("../controllers/userController");
 const userRouter = express.Router();
 const auth = require("../middlewares/auth");
 
 userRouter.post("/signup", signup);
 userRouter.post("/signin", signin);
 userRouter.post("/reportBugs", auth, reportBugs);
+userRouter.patch("/resetPassword", auth, resetPassword);
 
 module.exports = userRouter;
