971b89db23f2c764c2b487d14789abfd1ebfa137
tanishcq <tanishcqmehta@gmail.com>
2023-01-02 17:11:38 +0530

    added reportBugs route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { signin, signup } = require("../controllers/userController");
+const { signin, signup, reportBugs } = require("../controllers/userController");
 const userRouter = express.Router();
+const auth = require("../middlewares/auth");
 
 userRouter.post("/signup", signup);
 userRouter.post("/signin", signin);
+userRouter.post("/reportBugs", auth, reportBugs);
 
 module.exports = userRouter;
