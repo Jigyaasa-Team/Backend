a20067f773ffbc1762742ebb15a8e235dbd428f3
tanishcq <tanishcqmehta@gmail.com>
2023-01-03 01:12:02 +0530

    Defined mailing services using google APIs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,8 @@
 const userModel = require("../models/user");
 const bugModel = require("../models/bug");
 const bcrypt = require("bcrypt");
-// const nodemailer = require("nodemailer");
-// const randomString = require("randomstring");
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -96,14 +96,60 @@ const signin = async (req, res) => {
 
 const forgotPassword = async (req, res) => {
     try {
-        // const existingUser = await userModel.findOne({ email: req.body.email });
-        // if(existingUser) {
-        //     const randomstring = randomString.generate();
-        //     const data = await userModel.updateOne({email}, {$set: {passwordResetToken: randomstring}});
-        //     res.status(200).json({ message: "Please check your email to reset your password." });
-        // } else {
-        //     res.status(200).json({ message: "User doesn't exists!" });
-        // }
+        const existingUser = await userModel.findOne({ email: req.body.email });
+        if(existingUser) {
+            const CLIENT_ID = '1051862290422-ps3d8sptmg67vqgu5fli5852mb0r78p8.apps.googleusercontent.com';
+            const CLIENT_SECRET = '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';
+            const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+            const REFRESH_TOKEN = '1//04qxKA1is7WO9CgYIARAAGAQSNwF-L9Ir2O0Deb1s-I1ZjJYA79r_Yhn-w4FXQrUQTkMwAfIJz5waioMx2jKOcWddnRRg_FoD3pA';
+            
+            const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+            oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+            const secret = SECRET_KEY + existingUser.password;
+            const payload = {
+                email: existingUser.email,
+                id: existingUser.id
+            }
+            const token = jwt.sign(payload, secret, { expiresIn: '15m' });
+            // const link = `http://fms-backend-production-ce11.up.railway.app/forgot-password/${existingUser.id}/${token}`;
+            const link = `http://localhost:${process.env.PORT}/forgot-password/${existingUser.id}/${token}`;
+            console.log(link);
+
+            const sendMail = async () => {
+                try {
+                    const accessToken = await oAuth2Client.getAccessToken();
+
+                    const transport = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            type: 'OAuth2',
+                            user: 'tanishcqmehta.dev@gmail.com',
+                            clientId: CLIENT_ID,
+                            clientSecret: CLIENT_SECRET,
+                            refreshToken: REFRESH_TOKEN,
+                            accessToken
+                        }
+                    })
+
+                    const mailOptions = {
+                        from: 'FMS Development Team <tanishcqmehta.dev@gmail.com>',
+                        to: existingUser.email,
+                        subject: "Reset Password",
+                        text: `Click on the following link to reset your password : ${link}`,
+                        html: `<h2>Click on the following link to reset your password : ${link}</h2>`
+                    }
+
+                    const result = await transport.sendMail(mailOptions);
+                    return res.status(200).json({ message: "Please check your email to reset your password.", result });
+                } catch(err) {
+                    return res.status(400).json({ message: `Something went wrong! ${err}` });
+                }
+            };
+            sendMail();
+        } else {
+            res.status(200).json({ message: "User doesn't exists!" });
+        }
     } catch (err) {
         res.status(400).json({ message: `Something went wrong! ${err}` });
     }
@@ -135,6 +181,43 @@ const resetPassword = async (req, res) => {
     }
 };
 
+const resetPasswordFromLink = async (req, res, next) => {
+    const { id, token } = req.params;
+    const existingUser = await userModel.findById({ _id: id });
+    if(!existingUser) {
+        return res.status(400).json({ message: "Couldn't reset password, Not Found!" });
+    }
+
+    const secret = SECRET_KEY + existingUser.password;
+    try {
+        const payload = jwt.verify(token, secret); 
+        res.render("reset-password", { email: existingUser.email });
+    } catch (err) {
+        res.status(400).json({ message: `Something went wrong! ${err}` });
+    }
+};
+
+const verifyAndResetPassword = async (req, res, next) => {
+    const { id, token } = req.params;
+    const existingUser = await userModel.findById({ _id: id });
+    if(!existingUser) {
+        return res.status(400).json({ message: "Couldn't reset password, Not Found!" });
+    }
+
+    const secret = SECRET_KEY + existingUser.password;
+    try {
+        const payload = jwt.verify(token, secret); 
+        // res.render("reset-password", { email: existingUser.email });
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const userPassword = await userModel.findByIdAndUpdate({ _id: req.userId }, { password: hashedPassword }, { new: true });
+        return res.status(200).json({ message: "Password reset was successful!" , userPassword });
+    } catch (err) {
+        res.status(400).json({ message: `Something went wrong! ${err}` });
+    }
+};
+
 const reportBugs = async (req, res) => {
     try {
         const { description } = req.body;
@@ -154,4 +237,4 @@ const reportBugs = async (req, res) => {
     }
 };
 
-module.exports = { signin, signup, forgotPassword, reportBugs, resetPassword };
+module.exports = { signin, signup, forgotPassword, reportBugs, resetPassword, verifyAndResetPassword, resetPasswordFromLink };
